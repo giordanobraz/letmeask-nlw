41b67c5682c0aa3d2d3b764af40dcdba63e4344f
Giordano Bruno Silva <giordano@devplusagencia.com.br>
2021-06-24 21:46:29 -0300

    Commit Inicial - Aula 4

---
+++ src/hooks/useRoom.ts
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+import { database } from "../services/firebase";
+import { useAuth } from "./useAuth";
+type Questions = {
+  id: string;
+  author: {
+    nome: string;
+    avatar: string;
+  }
+  content: string;
+  isAnswered: boolean;
+  isHighlighted: boolean;
+  likeCount: number;
+  likeId: string | undefined;
+}
+
+type firebaseQuestions = Record<string, {
+  author: {
+    nome: string;
+    avatar: string;
+  }
+  content: string;
+  isAnswered: boolean;
+  isHighlighted: boolean;
+  likes: Record<string, {
+    authorId: string
+  }>
+}>
+export function useRoom(roomId: string) {
+  const { user } = useAuth();
+  const [questions, setQuestions] = useState<Questions[]>([]);
+  const [title, setTitle] = useState('');
+
+  useEffect(() => {
+    const roomRef = database.ref(`rooms/${roomId}`);
+    roomRef.on('value', room => {
+      const databaseRoom = room.val();
+      const databaseQuestions: firebaseQuestions = databaseRoom.questions ?? {};
+      const parsedQuestions = Object.entries(databaseQuestions).map(([key, value]) => {
+        return {
+          id: key,
+          content: value.content,
+          author: value.author,
+          isHighlighted: value.isHighlighted,
+          isAnswered: value.isAnswered,
+          likeCount: Object.values(value.likes ?? {}).length,
+          likeId: Object.entries(value.likes ?? {}).find(([key, like]) => like.authorId === user?.id)?.[0]
+        }
+      });
+      setTitle(databaseRoom.title);
+      setQuestions(parsedQuestions);
+    })
+
+    return () => {
+      roomRef.off('value');
+    }
+  }, [roomId, user?.id])
+
+  return { questions, title }
+}
---
+++ src/pages/AdminRoom.tsx
@@ -0,0 +1,77 @@
+import { Button } from '../components/Button';
+import { RoomCode } from '../components/RoomCode';
+import { Question } from '../components/Question';
+import { useHistory, useParams } from 'react-router-dom'
+import { useRoom } from '../hooks/useRoom';
+// import { useAuth } from '../hooks/useAuth';
+import imgLogo from '../assets/images/logo.svg'
+import deleteImg from '../assets/images/delete.svg'
+
+import '../styles/room.scss'
+import { database } from '../services/firebase';
+type RoomParams = {
+  id: string
+}
+
+export function AdminRoom() {
+  // const { user } = useAuth();
+  const history = useHistory();
+  const params = useParams<RoomParams>();
+  const roomId = params.id;
+  const { questions, title } = useRoom(roomId)
+
+  async function handleDeleteQuestion(questionId: string) {
+    if (window.confirm('Tem certeza que deseja remover a pergunta?')) {
+      await database.ref(`rooms/${roomId}/questions/${questionId}`).remove()
+    }
+  }
+
+  async function handleEndRoom() {
+    await database.ref(`rooms/${roomId}`).update({
+      endedAt: new Date()
+    })
+    history.push('/')
+  }
+
+  return (
+    <div id="page-room">
+      <header>
+        <div className="content">
+        <a href="/">
+          <img src={imgLogo} alt="Logo" />
+        </a>
+          <div>
+            <RoomCode code={roomId} />
+            <Button isOutlined onClick={handleEndRoom}>Encerrar Sala</Button>
+          </div>
+        </div>
+      </header>
+
+      <main>
+        <div className="room-title">
+          <h1>Sala {title}</h1>
+          {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
+        </div>
+
+        <div className="question-list">
+          {questions.map(question => {
+            return (
+              <Question
+                key={question.id}
+                content={question.content}
+                author={question.author}
+              >
+                <button
+                  type="button"
+                  onClick={() => handleDeleteQuestion(question.id)}
+                >
+                  <img src={deleteImg} alt="Remover Pergunta" />
+                </button>
+              </Question>
+            );
+          })}
+        </div>
+      </main>
+    </div>
+  );
+}
